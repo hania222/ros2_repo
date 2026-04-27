36be2c4d68f27631e7bbffe33c448ed5b2a81cb6
hania <haniahazem60@gmail.com>
2026-04-27 14:42:16 +0000

    remove

--- build/robot_bringup/colcon_command_prefix_setup_py.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/hania/project/install/robot_controllers/share/robot_controllers/package.sh"
-. "/home/hania/project/install/robot_description/share/robot_description/package.sh"
